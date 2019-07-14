3523fdaa44e470976869cfae48398139b0d5dee5
Chadtech <chadstearns1@gmail.com>
2019-07-14 14:25:35 +0200

    Big update

--- README.md
+++ README.md
@@ -8,16 +8,25 @@ The example in thie Ellie app is probably the best demonstration of what this pa
 
 # Whats this all about?
 
-Ive heard whats been referred to as 'the lie of css', which is that despite obstensibly being about style sheets its not really about styling. If you didnt know anything about html or css, it would kind of seem like html is about laying out and css is about styling. But you know, thats not really true. Both html and css are necessary for both laying out and styling. The concepts dont neatly correspond to the technologies. At this point in history html and css are just a big accident with misleading names. Im sure if we started from scratch we could make something better (something like the Elm package `mdgriffith/elm-ui`).
+Ive heard whats been referred to as 'the lie of css', which is that despite being an abbreviation for "style sheets" its not really about styling. If you didnt know anything about html or css, it would kind of seem like html is about laying out and css is about styling. Thats not really true. Both html and css are necessary for both laying out and styling. The different concepts dont neatly correspond to the different technologies. At this point in history html and css are just a big ugly accident with misleading names. Im sure if we started the internet from scratch we would make something better than html and css (something like the Elm package `mdgriffith/elm-ui`).
 
-But in practice, it helps to think about positioning as a separate task from styling. You should have separate blocks of html for laying out and styling. Css rules can get complicated and are liable to interfere with each other. Making one monster html element that both looks right and is well placed just doesnt work. To make good maintainable html you can take the simple approach of dedicating some html elements to just positioning and other html elements to just styling.
+But in practice, it helps to think about positioning as a separate thing from styling. You should have separate blocks of html just for laying things out and other blocks of html just for being styled. If you dont do this, and instead try to make make one monster html element that both looks right and is well placed, you will not have a fun time. It will look funny, it will not be scalable, and it will break very easily. To make good maintainable html you can take the simple approach of dedicating some html elements to just positioning and other html elements to just styling.
 
-Thats where css grids come in. Css grids just position stuff in a grid, then its up to you to style the stuff in the grid how you need. 
+Thats where css grids come in. Css grids just position stuff in a grid, then its up to you to style the stuff in the grid how you want. 
 
 # Whats in this package
 
-This package exposes a `row` function and a `column` function. A sequence of `row`s will naturally be organized vertically, and a sequence of `column`s in a row will naturally be organized horizontally. They are flex elements, so if you put only one `column` in a row, it will take up the whole row horizontally. Two columns will each take up 50% of the row horizontally.
+This package exposes a `row` function and a `column` function. A sequence of `row`s will naturally be organized vertically, and a sequence of `column`s in a row will naturally be organized horizontally. They are flex elements, so if you put only one `column` in a row, it will take up the whole row horizontally. If you have two unstyled column in a row they will naturally each take up 50% of the horizontal space.
 
-The type signatures of `row` and `column` are very similar to that of html functions in `elm/html`, with the exception that their first parameter is a `List Css.Style` instead of `List (Attribute msg)`. This is an opinionated api; layout html does not need attributes. You can however style the grid with your own `List Css.Style`, which will override the default styling of the grid components, such as in the cases where a column needs a fixed width or a row needs a margin.
+
+# This is an opinionated Api
+
+This api is meant to be used in one way, and it wont let itself be used in another way:
+- The type signatures of `row` and `column` are very similar to that of html functions in `elm/html`, with the exception that their first parameter is a `List Css.Style` instead of `List (Attribute msg)`. Layout html does not need attributes. You need attributes for thinks like event handlers, and data. Layout html shouldnt be used for that kind of thing.
+- The `column` function makes a `Column msg` and not an `Html msg`. The `row` function takes `List (Column msg)` and not `List (Html msg)`. They only work with each other. A row with one column is fine, and preferable to a row with many non-column children.
+
+You can however style the grid with your own `List Css.Style`, which will override the default styling of the grid components, such as in the case where a row or column needs needs a margin or padding.
+
+# Also
 
 `row`s and `column`s show up in the DOM as `<row></row>` and `<column></column>`.
--- src/Html/Grid.elm
+++ src/Html/Grid.elm
@@ -1,35 +1,65 @@
-module Html.Grid exposing (container, row, column)
+module Html.Grid exposing
+    ( row
+    , Column, column, mapColumn
+    , columnShrink, exactWidthColumn
+    , box
+    , keyedRow, keyedColumn
+    )
 
 {-|
 
+    import Css exposing (..)
     import Html.Grid as Grid
     import Html.Styled
 
     view : Html.Styled.Html msg
     view =
-        Grid.container
-            []
+        Grid.box
+            [ width (pct 100) ]
             [ Grid.row
                 []
                 [ Grid.column
                     []
-                    [ Html.text "Hello from upper left column" ]
+                    [ Html.text "Hello from upper left corner" ]
                 , Grid.column
                     []
-                    [ Html.text "Hello from upper right column" ]
+                    [ Html.text "Hello from upper right corner" ]
                 ]
             , Grid.row
                 []
                 [ Grid.column
                     []
-                    [ Html.text "Hello from lower left column" ]
+                    [ Html.text "Hello from lower left corner" ]
                 , Grid.column
                     []
-                    [ Html.text "Hello from lower right column" ]
+                    [ Html.text "Hello from lower right corner" ]
                 ]
             ]
 
-@docs container, row, column
+
+# Row
+
+@docs row
+
+
+# Column
+
+@docs Column, column, mapColumn
+
+
+# Column Styling
+
+@docs columnShrink, exactWidthColumn
+
+
+# Extras
+
+@docs box
+
+
+# Keyed
+
+@docs keyedRow, keyedColumn
 
 -}
 
@@ -38,53 +68,22 @@ import Html.Styled as Html
     exposing
         ( Attribute
         , Html
-        , node
         )
-import Html.Styled.Attributes exposing (css)
-
-
-{-| Just a container for grids. Its not necessary, but it will get you off to a good start.
-
-    -- containers are styled with the css:
-    --     margin: 0 auto;
+import Html.Styled.Attributes as Attr
+import Html.Styled.Keyed
 
 
 
+--
+
+
+{-| A Column, a horizontally positioned unit inside a row
 -}
-container : List Style -> List (Html msg) -> Html msg
-container styles =
-    node "container" [ css [ containerStyle, Css.batch styles ] ]
+type Column msg
+    = Column (Html msg)
 
 
-containerStyle : Style
-containerStyle =
-    [ margin2 zero auto ]
-        |> Css.batch
-
-
-{-| A row in a grid
-
-    -- rows are styled with the css:
-    --     display: flex;
-    --     flex-direction: row;
-
-
-
--}
-row : List Style -> List (Html msg) -> Html msg
-row styles =
-    node "row" [ css [ rowStyle, Css.batch styles ] ]
-
-
-rowStyle : Style
-rowStyle =
-    [ displayFlex
-    , flexDirection Css.row
-    ]
-        |> Css.batch
-
-
-{-| A column in a grid
+{-| A column in a row. Give `column` some html and it positions that html horizontally inside a `row`. You can also style it, overriding its default styles.
 
     -- columns are styled with the css:
     --     flex-basis: 100%;
@@ -94,15 +93,139 @@ rowStyle =
 
 
 -}
-column : List Style -> List (Html msg) -> Html msg
-column styles =
-    node "column" [ css [ columnStyle, Css.batch styles ] ]
+column : List Style -> List (Html msg) -> Column msg
+column styles children =
+    Html.node "column"
+        [ columnStyles styles ]
+        children
+        |> Column
 
 
-columnStyle : Style
-columnStyle =
-    [ flexBasis (pct 100)
-    , flex (int 1)
-    , displayFlex
+{-| `Column`s naturally fill up horizontal space. Adding `columnShrink` to a column will make it do the opposite, and shrink by default.
+
+    Grid.row
+        []
+        [ Grid.column
+            []
+            [ searchField model.searchFieldModel ]
+        , Grid.column
+            [ Grid.columnShrink ]
+            [ searchButton "Search" SearchClicked ]
+        ]
+
+    -- Will render something like..
+    -- [ --------- <Search field> --------- ] [ Search ]
+    -- |           Column                    | Column  |
+    -- | ---------------------- Row -------------------|
+
+-}
+columnShrink : Style
+columnShrink =
+    flex (int 0)
+
+
+{-| `Column`s naturally fill up horizontal space. but if instead you want it to have a specific width, use this
+
+    Grid.row
+        []
+        [ Grid.column
+            -- This column takes up exactly 50% of the rows width
+            [ Grid.exactWidthColumn (pct 50) ]
+            []
+        , Grid.column
+            -- This column takes whatever free horizontal space is available
+            []
+            []
+        , Grid.column
+            -- This column is exactly 100 pixels wide
+            [ Grid.exactWidthColumn (px 100) ]
+            []
+        ]
+
+-}
+exactWidthColumn : LengthOrAuto compatible -> Style
+exactWidthColumn width_ =
+    [ flex none
+    , width width_
     ]
         |> Css.batch
+
+
+{-| The same as a regular `column`, but with keyed content ( (See Html.Styled.Keyed for more info)[<https://package.elm-lang.org/packages/rtfeldman/elm-css/latest/Html-Styled-Keyed>][https://package.elm-lang.org/packages/rtfeldman/elm-css/latest/Html-Styled-Keyed] )
+-}
+keyedColumn : List Style -> List ( String, Html msg ) -> Column msg
+keyedColumn styles children =
+    Html.Styled.Keyed.node "column"
+        [ columnStyles styles ]
+        children
+        |> Column
+
+
+columnStyles : List Style -> Attribute msg
+columnStyles styles =
+    Attr.css
+        [ flexBasis (pct 100)
+        , flex (int 1)
+        , displayFlex
+        , Css.batch styles
+        ]
+
+
+{-| Just like `Html.map`, except for `Column`
+-}
+mapColumn : (a -> b) -> Column a -> Column b
+mapColumn f (Column html) =
+    Column <| Html.map f html
+
+
+columnToHtml : Column msg -> Html msg
+columnToHtml (Column html) =
+    html
+
+
+{-| A row in a grid. Sequential rows will stack vertically. A row takes a `List (Column msg)`, NOT `List (Html msg)`. `Column msg` inside the `row` are arranged horizontally.
+
+    -- rows are styled with the css:
+    --     display: flex;
+    --     flex-direction: row;
+
+
+
+-}
+row : List Style -> List (Column msg) -> Html msg
+row styles columns =
+    Html.node "row"
+        [ rowStyles styles ]
+        (List.map columnToHtml columns)
+
+
+{-| The same as a regular `row`, but with keyed columns ( (See Html.Styled.Keyed for more info)[<https://package.elm-lang.org/packages/rtfeldman/elm-css/latest/Html-Styled-Keyed>][https://package.elm-lang.org/packages/rtfeldman/elm-css/latest/Html-Styled-Keyed] )
+-}
+keyedRow : List Style -> List ( String, Column msg ) -> Html msg
+keyedRow styles columns =
+    Html.Styled.Keyed.node "row"
+        [ rowStyles styles ]
+        (List.map (Tuple.mapSecond columnToHtml) columns)
+
+
+rowStyles : List Style -> Attribute msg
+rowStyles extraStyles =
+    Attr.css
+        [ displayFlex
+        , flexDirection Css.row
+        , Css.batch extraStyles
+        ]
+
+
+{-| I find that when I write html, many html elements are just containers for other things. Containers either dont get any attributes at all, or just some styling attributes to position them or their content. This is just a helper for those simple containers. A `box` is supposed to be simple, invisible, and not interactive. It only takes a `List Style` instead of a `List (Attribute msg)`.
+
+    Grid.box
+        [ Css.width (pct 100) ]
+        [ Grid.row [] [ Grid.column [] [] ]
+        , Grid.row [] [ Grid.column [] [] ]
+        ]
+
+-}
+box : List Style -> List (Html msg) -> Html msg
+box styles =
+    Html.node "box" [ Attr.css styles ]
